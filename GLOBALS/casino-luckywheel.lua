498987e0ee34da1218cbc167fc993c7c4b1b993a
thecookietho <thecookietho@gmail.com>
2025-12-13 14:29:47 +0100

    added some globals, updated TODO

---
+++ GLOBALS/casino-luckywheel.lua
@@ -0,0 +1,45 @@
+Casino Lucky Wheel"
+ 
+-- Enhanced 1.72 ~ b1013.17 --
+ 
+// Lucky Wheel Globals
+Global_262145.f_26854 = 1  // Max. Spins Per Day
+Global_262145.f_26855 = TRUE // Enable Additional Spins
+Global_262145.f_37457 = 2 // Max. Spins Per Day w/ GTA+
+ 
+ 
+// Lucky Wheel OutCUM
+casino_lucky_wheel Local_143[PLAYER_ID * 5]
+ 
+----------------------------
+ 
+-- Legacy 1.72 ~ b3586 --
+ 
+// Lucky Wheel Globals
+Global_262145.f_26834  // Max. Spins Per Day
+Global_262145.f_26835 = TRUE // Enable Additional Spins
+ 
+// Lucky Wheel OutCUM
+casino_lucky_wheel Local_141[PLAYER_ID * 5]
+
+Value - Prize
+0 // Clothing
+1 // 2500 RP
+2 // $20000
+3 // 10000 Chips
+4 // 10% Discount Voucher
+5 // 5000 RP
+6 // $30000
+7 // 15000 Chips
+8 // Clothing
+9 // 7500 RP
+10 // 20000 Chips
+11 // Mystery Prize
+12 // Clothing
+13 // 10000 RP
+14 // $40000
+15 // 25000 Chips
+16 // Clothing
+17 // 15000 RP
+18 // Podium Vehicle
+19 // $50000
